b59af77f3b7ca3fc8b8b590016901c374c89ae39
xiuli <18729211546@163.com>
2019-03-24 09:20:14 +0800

    hxl

--- push.sh
+++ push.sh
@@ -3,9 +3,9 @@ echo "初始化中....."
 git add -A;
 read -p "输入日志,按Enter键跳过 :" log
 if  [ ! -n "$log" ] ;then
-    git commit -m "真懒";
+    git commit -m "vue";
 else git commit -m "${log}";
 fi;
-git pull;
+#git pull;
 git push ;
 echo "推送完成"
