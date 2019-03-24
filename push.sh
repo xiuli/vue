d10d5436f56e67051e2561eeee104f0ebf0444fb
xiuli <18729211546@163.com>
2019-03-24 09:16:49 +0800

    hxl

---
+++ push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "初始化中....."
+git add -A;
+read -p "输入日志,按Enter键跳过 :" log
+if  [ ! -n "$log" ] ;then
+    git commit -m "真懒";
+else git commit -m "${log}";
+fi;
+git pull;
+git push ;
+echo "推送完成"
